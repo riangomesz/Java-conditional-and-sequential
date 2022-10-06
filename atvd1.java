80eed6448fdeb4d79d0e59cd8baf277a4b2dbb8c
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-06 10:36:36 -0300

    Add files via upload

---
+++ atvd1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.text.DecimalFormat;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("##.##");
+		double valor;
+		
+		System.out.println("Informe o 1o valor");
+		valor = leitor.nextDouble();
+		System.out.println(+valor+" ao quadrado é: "+df.format(Math.pow(valor,2)));
+		System.out.println(+valor+" ao cubo é: "+df.format(Math.pow(valor,3)));
+		System.out.println("A raíz quadrada de " +valor+" é: "+df.format(Math.sqrt(valor)));
+		System.out.println("A raíz cúbica de " +valor+" é: "+df.format(Math.cbrt(valor)));
+	}
+}
