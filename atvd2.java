6161311d13f8eec3de497970a44c515f44e02af3
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-07 10:18:37 -0300

    Add files via upload

---
+++ atvd2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("##.##");
+		
+		float altura;
+		double pesoideal;
+		System.out.println("Informe sua altura em Metros:");
+		altura = leitor.nextFloat();
+		if(altura<=0||altura>2.10){
+		    System.out.println("Informe sua altura em metros corretamente");
+		}else{
+		    pesoideal = (72.7*altura)- 58;
+		    System.out.println("Seu peso ideal é: "+df.format(pesoideal)+" kg.");
+		}
+	}
+}
