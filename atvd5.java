7abd412357341986554b8cbb00fc3f9133af2eb6
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-10 20:08:58 -0300

    Add files via upload

---
+++ atvd5.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.text.DecimalFormat;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("##.##");
+		
+		int a,b,c; double x1,x2;
+		double delta;
+		
+		System.out.println("Nesse programa você irá tirar as raízes da equação.");
+		System.out.println("Informe o A: ");
+		a=sc.nextInt();
+		System.out.println("Informe o B: ");
+		b=sc.nextInt();
+		System.out.println("Informe o C: ");
+		c=sc.nextInt();
+		
+		//fórmula para o delta
+	    delta = (Math.pow(b, 2)) - (4 * a * c);
+	  //delta =(Math.pow(b, 2)) - (4 * a * c)
+	    System.out.println("O valor do Delta é: "+df.format(delta));
+		
+		if(delta > 0){
+		    x1 = ((-b + Math.sqrt (delta))  / (2 * a));
+		      // ((-b + Math.sqrt(delta)) / (2 * a));
+		    x2 = ((-b - Math.sqrt(delta)) / (2 * a));
+		     //  ((-b - Math.sqrt(delta)) / (2 * a));
+		    System.out.println("Valor de x1: "+df.format(x1));
+		    System.out.println("Valor de x2: "+df.format(x2));
+		}else{
+		    System.out.println("Não pode ser calculado pois o delta está negativo.");
+		}
+	}
+}
