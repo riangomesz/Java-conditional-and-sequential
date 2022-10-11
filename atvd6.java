c5341cf0f6378baa5c41a96f0c4c0dcdbe31121f
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-11 13:16:34 -0300

    Add files via upload

---
+++ atvd6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		int num1,num2;
+		
+	    System.out.println("Informe o 1º valor: ");
+	    num1 = leitor.nextInt();
+	    System.out.println("Informe o 1º valor: ");
+	    num2 = leitor.nextInt();
+	    
+	    if(num1>num2){
+	        System.out.println("\n"+num1+" é maior que o "+num2+".");
+	        System.out.println("\n"+num2+" é menor que o "+num1+".");
+	    }
+	    if(num2>num1){
+	        System.out.println("\n"+num2+" é maior que o "+num1+".");
+	        System.out.println("\n"+num1+" é menor que o "+num2+".");
+	    }
+	}
+}
